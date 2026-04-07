36efeebd3548941aad00624c8dc6330edae01a8f
Nick Minor <nicholasrminor@gmail.com>
2026-04-07 14:31:27 -0500

    making visual multi mode in neovim a little more intuitive and less conflicting with macos keybinds

--- .config/neovim/plugins/visual-multi.nix
+++ .config/neovim/plugins/visual-multi.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }:
 {
+  globals.VM_maps = {
+    "Add Cursor Down" = "<M-j>";
+    "Add Cursor Up" = "<M-k>";
+  };
+
+  extraConfigVim = ''
+    nmap <M-Down> <Plug>(VM-Add-Cursor-Down)
+    nmap <M-Up> <Plug>(VM-Add-Cursor-Up)
+  '';
+
   extraPlugins = [
     pkgs.vimPlugins.vim-visual-multi
   ];
